c6ee29b288a864d615891cc1c15bf9d327a6385a
Atomsphere <m.w.maupin@gmail.com>
2017-10-18 19:44:01 -0500

    refactor to better match problem

--- LinearRegression/Source.cpp
+++ LinearRegression/Source.cpp
@@ -7,21 +7,20 @@ using namespace std;
 
 
 struct Mpg {
-	double mpg;
-	double cyl;
-	double dis;
-	double hp;
-	double w;
-	double a;
+	double y;
+	double x[5];
 };
 
 Mpg uLimit, lLimit;
+const float ALPHA = .01;
 
 void printTest(vector<Mpg> vec) {
 	for (int i = 0; i < vec.size(); ++i) {
-		cout << vec[i].mpg << " - " << vec[i].cyl << " - " << vec[i].dis
-			<< " - " << vec[i].hp << " - " << vec[i].w << " - " << vec[i].a
-			<< endl;
+		cout << vec[i].y << " - ";
+		for (int j = 0; j < 5; ++j) {
+			cout << vec[i].x[j] << " - ";
+		}
+		cout << endl;
 	}
 }
 
@@ -29,14 +28,12 @@ void printTest(vector<Mpg> vec) {
 //normalize: normalizes the data using feature scaling
 //returns a copy of the data vector with normalized data
 //******************************
-vector<Mpg> normalize(vector<Mpg> vec, Mpg uLimit, Mpg lLimit) {
+vector<Mpg> normalize(vector<Mpg> vec) {
 	for (int i = 0; i < vec.size(); ++i) {
-		vec[i].mpg = (vec[i].mpg - lLimit.mpg) / (uLimit.mpg - lLimit.mpg);
-		vec[i].cyl = (vec[i].cyl - lLimit.cyl) / (uLimit.cyl - lLimit.cyl);
-		vec[i].dis = (vec[i].dis - lLimit.dis) / (uLimit.dis - lLimit.dis);
-		vec[i].hp = (vec[i].hp - lLimit.hp) / (uLimit.hp - lLimit.hp);
-		vec[i].w = (vec[i].w - lLimit.w) / (uLimit.w - lLimit.w);
-		vec[i].a = (vec[i].a - lLimit.a) / (uLimit.a - lLimit.a);
+		vec[i].y = (vec[i].y - lLimit.y) / (uLimit.y - lLimit.y);
+		for (int j = 0; j < 5; ++j) {
+			vec[i].x[j] = (vec[i].x[j] - lLimit.x[j]) / (uLimit.x[j] - lLimit.x[j]);
+		}
 	}
 	return vec;
 }
@@ -47,52 +44,37 @@ vector<Mpg> normalize(vector<Mpg> vec, Mpg uLimit, Mpg lLimit) {
 //*******************
 vector<Mpg> bounds(vector<Mpg> vec) {
 	
-	
-	lLimit.mpg = DBL_MAX;
-	lLimit.cyl = DBL_MAX;
-	lLimit.dis = DBL_MAX;
-	lLimit.hp = DBL_MAX;
-	lLimit.w = DBL_MAX;
-	lLimit.a = DBL_MAX;
-
-	uLimit.mpg = 0;
-	uLimit.cyl = 0;
-	uLimit.dis = 0;
-	uLimit.hp = 0;
-	uLimit.w = 0;
-	uLimit.a = 0;
-
+	lLimit.y = DBL_MAX;
+	uLimit.y = 0;
+	for (int i = 0; i < 5; ++i) {
+		lLimit.x[i] = DBL_MAX;
+		uLimit.x[i] = 0;
+	}
 
 	for (int i = 0; i < vec.size(); ++i) {
-		if (lLimit.mpg > vec[i].mpg)
-			lLimit.mpg = vec[i].mpg;
-		if (lLimit.cyl > vec[i].cyl)
-			lLimit.cyl = vec[i].cyl;
-		if (lLimit.dis > vec[i].dis)
-			lLimit.dis = vec[i].dis;
-		if (lLimit.hp > vec[i].hp)
-			lLimit.hp = vec[i].hp;
-		if (lLimit.w > vec[i].w)
-			lLimit.w = vec[i].w;
-		if (lLimit.a > vec[i].a)
-			lLimit.a = vec[i].a;
+		if (lLimit.y > vec[i].y)
+			lLimit.y = vec[i].y;
+		for (int j = 0; j < 5; ++j) {
+			if (lLimit.x[j] > vec[i].x[j])
+				lLimit.x[j] = vec[i].x[j];
+		}
 
-		if (uLimit.mpg < vec[i].mpg)
-			uLimit.mpg = vec[i].mpg;
-		if (uLimit.cyl < vec[i].cyl)
-			uLimit.cyl = vec[i].cyl;
-		if (uLimit.dis < vec[i].dis)
-			uLimit.dis = vec[i].dis;
-		if (uLimit.hp < vec[i].hp)
-			uLimit.hp = vec[i].hp;
-		if (uLimit.w < vec[i].w)
-			uLimit.w = vec[i].w;
-		if (uLimit.a < vec[i].a)
-			uLimit.a = vec[i].a;
+		if (uLimit.y < vec[i].y)
+			uLimit.y = vec[i].y;
+		for (int j = 0; j < 5; ++j) {
+			if (uLimit.x[j] < vec[i].x[j])
+				uLimit.x[j] = vec[i].x[j];
+		}
 	}
-	return normalize(vec, uLimit, lLimit);
+		return normalize(vec);
 }
 
+
+void gradient(vector<Mpg> vec, double theta[5]) {
+	while (1) {
+
+	}
+}
 
 int main() {
 	vector<Mpg> data;
@@ -103,6 +85,7 @@ int main() {
 	Mpg temp;
 	string line;
 	string temp1;
+	double theta[5] = { 0, 0, 0, 0, 0 };
 
 	inFile.open("linear_regression_data-1.txt");
 
@@ -116,42 +99,38 @@ int main() {
 
 		if (line.at(0) == '?') {
 			getline(iss, temp1, '\t');
-			temp.mpg = -1;
-			getline(iss, temp1, '\t');
-			temp.cyl = stod(temp1);
-			getline(iss, temp1, '\t');
-			temp.dis = stod(temp1);
-			getline(iss, temp1, '\t');
-			temp.hp = stod(temp1);
-			getline(iss, temp1, '\t');
-			temp.w = stod(temp1);
+			temp.y = -1;
+			for (int i = 0; i < 4; ++i) {
+				getline(iss, temp1, '\t');
+				temp.x[i] = stod(temp1);
+			}
 			getline(iss, temp1);
-			temp.a = stod(temp1);
+			temp.x[4] = stod(temp1);
 
 			test.push_back(temp);
 		}
 		else {
 			getline(iss, temp1, '\t');
-			temp.mpg = stod(temp1);
-			getline(iss, temp1, '\t');
-			temp.cyl = stod(temp1);
-			getline(iss, temp1, '\t');
-			temp.dis = stod(temp1);
-			getline(iss, temp1, '\t');
-			temp.hp = stod(temp1);
-			getline(iss, temp1, '\t');
-			temp.w = stod(temp1);
+			temp.y = stod(temp1);
+			for (int i = 0; i < 4; ++i) {
+				getline(iss, temp1, '\t');
+				temp.x[i] = stod(temp1);
+			}
 			getline(iss, temp1);
-			temp.a = stod(temp1);
+			temp.x[4] = stod(temp1);
 			
 			data.push_back(temp);
 		}
 	}
 	
-	
 
-	
 
+	nData = bounds(data);
+	nTest = normalize(test); 	
+	cout << endl;
+	printTest(nTest);
+	cout << endl << endl;
+	printTest(nData);
 	return 0;
 
 }
